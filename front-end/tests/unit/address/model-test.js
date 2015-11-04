fb3dacf9e2d1f82b44d779811c42764492be7872
troy.steuwer <troy.steuwer@bookit.com>
2015-11-03 20:15:49 -0500

    updating unit tests

--- front-end/tests/unit/address/model-test.js
+++ front-end/tests/unit/address/model-test.js
@@ -1,12 +1,83 @@
-import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+import {
+	moduleForModel,
+	test
+} from 'ember-qunit';
+
+const {
+	get
+} = Ember;
 
 moduleForModel('address', 'Unit | Model | address', {
   // Specify the other units that are required for this test.
 	needs: ['model:person']
 });
 
-test('it exists', function(assert) {
-  var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
+test('attributes should be set', function(assert) {
+	assert.expect(6);
+
+	const updated = new Date();
+
+  const model = this.subject({
+		streetName: '123 Main St',
+		city: 'Twiddly Dee',
+		state: 'Florida',
+		zipCode: '12345',
+		country: 'US',
+		lastUpdated: updated
+	});
+
+  assert.equal(get(model, 'streetName'), '123 Main St', 'street name should be set');
+  assert.equal(get(model, 'city'), 'Twiddly Dee', 'city should be set');
+  assert.equal(get(model, 'state'), 'Florida', 'state hould be set');
+  assert.equal(get(model, 'zipCode'), '12345', 'zip code hould be set');
+	assert.equal(get(model, 'country'), 'US', 'country should be set');
+	assert.equal(get(model, 'lastUpdated' ), updated, 'lastUpdated should be a date');
+});
+
+test('should have a person relationship', function(assert) {
+	assert.expect(4);
+
+	const Address = this.store().modelFor('address');
+	const relationships = get(Address, 'relationshipsByName');
+	const person = relationships.get('person');
+
+	assert.ok(person.isRelationship, 'should have a relationship');
+	assert.ok(person.options.async, 'should be an async relationship');
+	assert.equal(person.key, 'person', 'has a relationship to person');
+	assert.equal(person.kind, 'belongsTo', 'address should belong to address');
+});
+
+test('default values should be set', function(assert) {
+	const model = this.subject({
+		streetName: '123 Main St',
+		city: 'Twiddly Dee',
+		state: 'Florida',
+		zipCode: '12345',
+		country: 'US'
+	});
+
+	assert.ok(get(model, 'lastUpdated') instanceof Date, 'lastUpdated should have a default Date object');
+});
+
+test('computed properties should work - fully-filled', function(assert) {
+	const model = this.subject({
+		streetName: '123 Main St',
+		city: 'Twiddly Dee',
+		zipCode: '12345',
+		country: 'US'
+	});
+
+	assert.equal(get(model, 'fullAddress'), '123 Main St, Twiddly Dee, 12345', 'Full name should be computed');
+});
+
+test('computed properties should work - semi-filled', function(assert) {
+	const model = this.subject({
+		streetName: '123 Main St',
+		city: 'Twiddly Dee',
+		state: 'Florida',
+		country: 'US'
+	});
+
+	assert.equal(get(model, 'fullAddress'), '123 Main St, Twiddly Dee, Florida', 'Full name should be computed');
 });
--- front-end/tests/unit/person/model-test.js
+++ front-end/tests/unit/person/model-test.js
@@ -1,12 +1,71 @@
-import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+import {
+	moduleForModel,
+	test
+} from 'ember-qunit';
+
+const {
+	get
+} = Ember;
 
 moduleForModel('person', 'Unit | Model | person', {
   // Specify the other units that are required for this test.
 	needs: ['model:address']
 });
 
-test('it exists', function(assert) {
-  var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
+test('attributes should be set', function(assert) {
+	assert.expect(5);
+
+	const created = new Date();
+	const modified = new Date();
+
+  const model = this.subject({
+		firstName: 'Jane',
+		lastName: 'Doe',
+		age: 22,
+		createdAt: created,
+		lastModifiedAt: modified
+	});
+
+  assert.equal(get(model, 'firstName'), 'Jane', 'First name should be set');
+  assert.equal(get(model, 'lastName'), 'Doe', 'Last name should be set');
+  assert.equal(get(model, 'lastName'), 'Doe', 'Last name should be set');
+	assert.equal(get(model, 'createdAt'), created, 'Created at should be set');
+	assert.equal(get(model, 'lastModifiedAt' ), modified, 'Modified at should be set');
+});
+
+test('should have an address relationship', function(assert) {
+	assert.expect(4);
+
+	const Person = this.store().modelFor('person');
+	const relationships = get(Person, 'relationshipsByName');
+	const address = relationships.get('address');
+
+	assert.ok(address.isRelationship, 'should have a relationship');
+	assert.ok(address.options.async, 'should be an async relationship');
+	assert.equal(address.key, 'address', 'has a relationship to address');
+	assert.equal(address.kind, 'belongsTo', 'address should belong to person');
+});
+
+test('default values should be set', function(assert) {
+	assert.expect(2);
+
+	const model = this.subject({
+		firstName: 'Jane',
+		lastName: 'Doe',
+		age: 22
+	});
+
+	assert.ok(get(model, 'createdAt') instanceof Date, 'createdAt should have a default Date object');
+	assert.ok(get(model, 'lastModifiedAt') instanceof Date, 'lastModifiedAt should have a default Date object');
+});
+
+test('computed properties should work', function(assert) {
+	const model = this.subject({
+		firstName: 'Jane',
+		lastName: 'Doe',
+		age: 22
+	});
+
+	assert.equal(get(model, 'fullName'), 'Jane Doe', 'Full name should be computed');
 });
